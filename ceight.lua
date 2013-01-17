3d011ad69cbcf045ad50f102c3c04d9b533eac24
Swyter <690187+Swyter@users.noreply.github.com>
2013-01-18 00:03:41 +0100

    *cleanup, use structs and starting with the interpreter

--- ceight.lua
+++ ceight.lua
@@ -3,8 +3,17 @@
 
 -- created by swyter; released under MIT-like terms.
 
-  ffi=require'ffi'
-  mem=ffi.new('unsigned char[?]', 0xfff)
+  local ffi=require'ffi'
+  local vm=ffi.new([[
+    struct{
+      unsigned char vid[64*32];                                     // display resolution is 64×32 pixels, and color is monochrome
+      unsigned char mem[0xfff];                                     // from 200h to FFFh, making for 3,584 bytes
+      unsigned int v0,v1,v2,v3,v4,v5,v6,v7,v8,v9,va,vb,vc,vd,ve,vf; // 16 8-bit data registers named from V0 to VF
+      unsigned int i;                                               // the address register, which is named I, is 16 bits wide
+    }
+  ]])
+  
+  local mem_base=0x200
 
   print("Starting off...")
   
@@ -13,20 +22,12 @@
     while true do
       local byte=f:read(1)
       c=(c or 0)+1
-      if not byte or ((0x200-1)+c)>0xfff then break end
-      print(string.format("%02X ",string.byte(byte)),c)
-      mem[(0x200-1)+c]=string.byte(byte)
+      if not byte or ((mem_base-1)+c)>0xfff then break end
+      vm.mem[(mem_base-1)+c]=string.byte(byte)
     end
     print("ROM loaded...")
   end
-
   
-    local t=0
-    while true do
-      if (0x200+t)>(0x200+c) then break end
-      for b=0,9 do
-        io.write(string.format("%02X ", mem[0x200+t+b]))
-      end
-      io.write("\n")
-      t=t+10
-    end
+  
+  io.write(string.format("%02X ", vm.mem[0x200]))
+  io.write(string.format("%02X ", vm.mem[0x201]))
