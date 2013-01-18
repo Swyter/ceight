52bd9847e70fc3e3438fe75b03d7c9d50b248ebb
Swyter <690187+Swyter@users.noreply.github.com>
2013-01-18 19:30:20 +0100

    *opcode mask recovery and basic serialization

--- ceight.lua
+++ ceight.lua
@@ -2,7 +2,8 @@
 --         written completely in Lua.
 
 -- created by swyter; released under MIT-like terms.
-
+  local opc=require'c8o'
+  local bit=require'bit'
   local ffi=require'ffi'
   local vm=ffi.new([[
     struct{
@@ -25,9 +26,58 @@
       if not byte or ((mem_base-1)+c)>0xfff then break end
       vm.mem[(mem_base-1)+c]=string.byte(byte)
     end
-    print("ROM loaded...")
+    print(string.format("ROM loaded... %d bytes",c))
+    f:close()
   end
   
   
-  io.write(string.format("%02X ", vm.mem[0x200]))
-  io.write(string.format("%02X ", vm.mem[0x201]))
+  
+  function toBits(num)
+    -- returns a table of bits, least significant first.
+    local t={} -- will contain the bits
+    while num>0 do
+        rest=math.fmod(num,2)
+        t[#t+1]=rest
+        num=(num-rest)/2
+    end
+    return string.rep(0,16-#t)..table.concat(t)
+  end
+  
+  
+  f = assert(io.open("_disasm", "w"))
+  --[[
+  for x=0,c+1,2 do
+  
+    f:write(string.format("%02X | ", 0x200+x))
+    
+    hexcomp=bit.lshift(vm.mem[0x200+x],8)+vm.mem[0x200+x+1]
+    f:write(string.format("%04X -> ", hexcomp))
+    f:write(string.format("%s -> ",   toBits(hexcomp)))
+    
+    for op,desc in pairs(opc) do
+      if bit.band(tonumber(op:gsub("[X|Y|N]","0"),16),hexcomp) ~=0 then
+        f:write(op.."  ")
+      end
+    end
+    
+    
+    
+    f:write('\n')
+  
+  end]]
+  
+    for op,desc in pairs(opc) do
+        f:write(op)
+        f:write('  ')
+        mask=tonumber(op:gsub("[^(X|Y|N)]","F"):gsub("[X|Y|N]","0"),16)
+        f:write(string.format("%04X", mask))
+        f:write('  ')
+        
+        f:write(toBits(mask))
+        --f:write(string.format("%04X", bit.band(tonumber(op:gsub("[X|Y|N]","6"),16), tonumber(mask,16))))
+        f:write('\n')
+    end
+  
+  f:close()
+  
+  
