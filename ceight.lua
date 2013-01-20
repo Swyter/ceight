3d1efa91422a3e65a5bc1a77bccee4f37b2e2de1
Swyter <Swyter@Portatil>
2013-01-20 17:13:54 +0100

    *refactor and streamline the way opcode args are handled. still not happy with it though *glyph sprites

--- ceight.lua
+++ ceight.lua
@@ -13,8 +13,26 @@
       unsigned int i;                                               // the address register, which is named I, is 16 bits wide
     }
   ]])
-  
+
   local mem_base=0x200
+  
+  --predefined array of sprites (glyphs)
+  vm.mem={ 0xF0, 0x90, 0x90, 0x90, 0xF0, -- 0
+           0x20, 0x60, 0x20, 0x20, 0x70, -- 1
+           0xF0, 0x10, 0xF0, 0x80, 0xF0, -- 2
+           0xF0, 0x10, 0xF0, 0x10, 0xF0, -- 3
+           0x90, 0x90, 0xF0, 0x10, 0x10, -- 4
+           0xF0, 0x80, 0xF0, 0x10, 0xF0, -- 5
+           0xF0, 0x80, 0xF0, 0x90, 0xF0, -- 6
+           0xF0, 0x10, 0x20, 0x40, 0x40, -- 7
+           0xF0, 0x90, 0xF0, 0x90, 0xF0, -- 8
+           0xF0, 0x90, 0xF0, 0x10, 0xF0, -- 9
+           0xF0, 0x90, 0xF0, 0x90, 0x90, -- A
+           0xE0, 0x90, 0xE0, 0x90, 0xE0, -- B
+           0xF0, 0x80, 0x80, 0x80, 0xF0, -- C
+           0xE0, 0x90, 0x90, 0x90, 0xE0, -- D
+           0xF0, 0x80, 0xF0, 0x80, 0xF0, -- E
+           0xF0, 0x80, 0xF0, 0x80, 0x80} -- F
 
   print("Starting off...")
   
@@ -50,12 +68,24 @@
       t = reverse(t)
       return table.concat(t)..("-"):rep(16-#t)
   end
-  function unpack(base,start,len)
-    local  num=bit.band(base, tonumber(('0'):rep(start)..
-                                       ('F'):rep(len)..
-                                       ('0'):rep((4)-(start+len)), 16))
+  function buildargs(base,start,len)
+    local  num=tonumber(('0'):rep(start)..
+                        ('F'):rep(len)..
+                        ('0'):rep((4)-(start+len)), 16)
                                        
-    return string.format("%X", bit.rshift(num, ((4)-(start+len))*4))
+    return {['mask']=num, ['shift']=((4)-(start+len))*4}
+  end
+  function unpack(base,mask,shift)
+    local num=bit.band(base, mask)
+    return bit.rshift(num, shift)
+  end
+  function print_r (t, indent) -- alt version, abuse to http://richard.warburton.it
+    local indent=indent or ''
+    for key,value in pairs(t) do
+      f:write(indent,'[',tostring(key),']') 
+      if type(value)=="table" then f:write(':\n') print_r(value,indent..'\t')
+      else f:write(' = ',tostring(value),'\n') end
+    end
   end
   
   f = assert(io.open("_disasm", "w"))
@@ -63,18 +93,20 @@
   --preprocess opcodes
   for op,opn in pairs(opc) do
   
-      mask=tonumber(op:gsub("[^(X|Y|N)]","F"):gsub("[X|Y|N]","0"),16)
-      base=bit.band(tonumber(op:gsub("[X|Y|N]","0"),16), mask)
+      local mask=         tonumber(op:gsub("[^(X|Y|N)]","F"):gsub("[X|Y|N]","0"),16)
+      local base=bit.band(tonumber(op:gsub(  "[X|Y|N]", "0"),                    16), mask)
+      
+      local args={}
+      if op:find("^.X..$")       then args.x=(buildargs(by,1,1)) end
+      if op:find("^..Y.$")       then args.y=(buildargs(by,2,1)) end
+      if op:find("^.NNN$")       then args.n=(buildargs(by,1,3)) end
+      if op:find("^.[^N]NN$")    then args.n=(buildargs(by,2,2)) end
+      if op:find("^.[^N][^N]N$") then args.n=(buildargs(by,3,1)) end
 
       opc[op]['base']=base
       opc[op]['mask']=mask
-      opc[op]['exec']=function(op, by) arg=''
-          if op:find("^.X..$")       then arg=arg..(", x·"..unpack(by,1,1)) end
-          if op:find("^..Y.$")       then arg=arg..(", y·"..unpack(by,2,1)) end
-          if op:find("^.NNN$")       then arg=arg..(", n·"..unpack(by,1,3)) end
-          if op:find("^.[^N]NN$")    then arg=arg..(", n·"..unpack(by,2,2)) end
-          if op:find("^.[^N][^N]N$") then arg=arg..(", n·"..unpack(by,3,1)) end
-      end
+      opc[op]['args']=args
+      opc[op]['exec']=function(...) print_r(...) end
       
   end
   
@@ -92,9 +124,13 @@
       
       for op,opn in pairs(opc) do
         if bit.band(by,opn.mask)==opn.base then
-          opn.exec(op,by)
-          f:write(string.format("%4X| %4X  %5s%s", pc, by, opn.me, arg))
-          f:write((' '):rep(16-(arg:len() or 0))..' ;'..opn.desc..'\n')
+          
+          local arg={}
+          for nm,ay in pairs(opn.args) do arg[nm]=unpack(by,ay.mask,ay.shift) end
+          
+          f:write(string.format("%4X| %4X  %5s\n", pc, by, opn.me ))
+          opn.exec(arg)
+          
           break end
       end
 
@@ -103,4 +139,9 @@
   
   f:close()
   
-  
+  --for _=0,32/2 do
+  --  for _=0,64/2 do
+  --     io.write("Üß")
+  --  end
+  --  io.write("\n")
+  --end
